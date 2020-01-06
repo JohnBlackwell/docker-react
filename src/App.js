b849ba9cf33d34b2c2872ea38e7ea2086f309078
JohnBlackwell <john.blackwell19@gmail.com>
2020-01-05 20:23:12 -0500

    fix

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and word .
+          HELLO <code>src/App.js</code> and word .
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn REACE NATIVE
+          Learn React
         </a>
       </header>
     </div>
